5eee3407b8ceaa9102bd345b75ed95115fceced0
Greg Landowski <greglando@gmail.com>
2016-01-02 19:16:33 -0800

    - GuildContext structure finished, guild roster and guild history added  - Champ title id added to enum

--- GWCA/GWStructures.h
+++ GWCA/GWStructures.h
@@ -412,8 +412,31 @@ namespace GWAPI {
 
 		using AgentEffectsArray = gw_array<AgentEffects>;
 
+
+		struct GHKey { DWORD key[4]; };
+
+		struct GuildPlayer {
+			void* vtable;
+			wchar_t* name_ptr; // ptr to invitedname, why? dunno
+			wchar_t invitedname[20]; // name of character that was invited in
+			wchar_t currentname[20]; // name of character currently being played
+			wchar_t invitorname[20]; // name of character that invited player
+			DWORD invitetime; // time in ms from game creation ??
+			wchar_t promotername[20]; // name of player that last modified rank
+		};
+
+		using GuildRoster = gw_array<GuildPlayer*>;
+
+		struct GuildHistoryEvent {
+			DWORD time1; // Guessing one of these is time in ms
+			DWORD time2;
+			wchar_t name[0x100]; // Name of added/kicked person, then the adder/kicker, they seem to be in the same array
+		};
+
+		using GuildHistory = gw_array<GuildHistoryEvent*>;
+
 		struct Guild {
-			DWORD GuildHallKey[4];
+			GHKey key;
 
 			inline wchar_t* name() const { return (wchar_t*)(this + 0x30); }
 			inline wchar_t* tag() const { return (wchar_t*)(this + 0x80); }

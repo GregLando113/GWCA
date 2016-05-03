b211fa5913a4e2807f512c1a4ecea6cde442bb91
Giorgio Gori <ggori@live.com>
2016-05-02 20:15:56 -0700

    added a Skillbar.IsValid()

--- GWCA/GWStructures.h
+++ GWCA/GWStructures.h
@@ -384,7 +384,7 @@ namespace GWCA {
 			DWORD Recharge;					// 0008					
 			DWORD SkillId;					// 000C						see GWConst::SkillIds
 			DWORD Event;					// 0010	s
-			long GetRecharge() const {			// returns recharge time remaining in milliseconds, or 0 if recharged
+			long GetRecharge() const {		// returns recharge time remaining in milliseconds, or 0 if recharged
 				if (Recharge == 0) return 0;
 				return Recharge - MemoryMgr::GetSkillTimer();
 			}
@@ -392,12 +392,13 @@ namespace GWCA {
 		struct Skillbar {						// total : BC BYTEs
 			Skillbar() : AgentId(0) {}
 			DWORD AgentId;						// 0000						id of the agent whose skillbar this is
-			SkillbarSkill Skills[8];						// 0004
+			SkillbarSkill Skills[8];			// 0004
 			DWORD Disabled;
 			BYTE unknown1[8];					// 00A8	|--8 BYTEs--|
 			DWORD Casting;						// 00B0
 			BYTE unknown2[8];					// 00B4	|--8 BYTEs--|
 			static Skillbar Nil() { return Skillbar(); }
+			bool IsValid() const { return AgentId > 0; }
 		};
 
 		typedef gw_array<Skillbar> SkillbarArray;

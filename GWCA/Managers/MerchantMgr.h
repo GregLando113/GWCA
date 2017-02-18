c0542b7533b410f3527c8a724a576f9c26eee1a3
HasKha <ggori@live.com>
2017-02-18 14:00:09 -0800

    minor fixes in syntax and formatting

--- GWCA/Managers/MerchantMgr.h
+++ GWCA/Managers/MerchantMgr.h
@@ -6,50 +6,50 @@
 #include <GWCA\GameEntities\Item.h>
 
 namespace GW {
-    
-    struct TransactionInfo {
-        DWORD itemcount;
-        DWORD* itemids;
-        DWORD* itemquantities;
-    };
-    
-    struct QuoteInfo {
-        DWORD unknown; // = 0
-        DWORD itemcount;
-        DWORD* itemids;
-    };
-    
-    enum class TransactionType : DWORD {
-        MerchantBuy = 0x1,
-        CollectorBuy,
-        CrafterBuy,
-        WeaponsmithCustomize,
-        
-        MerchantSell = 0xB,
-        TraderBuy,
-        TraderSell,
-        
-        UnlockRunePriestOfBalth= 0xF
-    };
-    
-    class MerchantMgr : public GWCAManager<MerchantMgr> {
-        friend class GWCAManager<MerchantMgr>;
-        
-        public:
-        
-        void RestoreHooks() override;
-        
-        void TransactItems(TransactionType type,
-                           DWORD gold_give,
-                           TransactionInfo give,
-                           DWORD gold_recv,
-                           TransactionInfo recv
-                           );
-        
-        void RequestQuote(TransactionType type,
-                          QuoteInfo give,
-                          QuoteInfo recv
-                          );
-        
-        DWORD GetQuotedItemPrice();
-    }
+
+	struct TransactionInfo {
+		DWORD itemcount;
+		DWORD* itemids;
+		DWORD* itemquantities;
+	};
+
+	struct QuoteInfo {
+		DWORD unknown; // = 0
+		DWORD itemcount;
+		DWORD* itemids;
+	};
+
+	enum TransactionType : DWORD {
+		MerchantBuy = 0x1,
+		CollectorBuy,
+		CrafterBuy,
+		WeaponsmithCustomize,
+
+		MerchantSell = 0xB,
+		TraderBuy,
+		TraderSell,
+
+		UnlockRunePriestOfBalth = 0xF
+	};
+
+	class MerchantMgr : public GWCAManager<MerchantMgr> {
+		friend class GWCAManager<MerchantMgr>;
+
+	public:
+		MerchantMgr();
+
+		void TransactItems(TransactionType type,
+			DWORD gold_give,
+			TransactionInfo give,
+			DWORD gold_recv,
+			TransactionInfo recv
+		);
+
+		void RequestQuote(TransactionType type,
+			QuoteInfo give,
+			QuoteInfo recv
+		);
+
+		//DWORD GetQuotedItemPrice();
+	};
+}
--- GWCA/src/MerchantMgr.cpp
+++ GWCA/src/MerchantMgr.cpp
@@ -4,95 +4,79 @@
 #include <GWCA\Managers\ItemMgr.h>
 #include <GWCA\Managers\MemoryMgr.h>
 #include <GWCA\Managers\StoCMgr.h>
+#include <GWCA\Packets\StoC.h>
 
 
 typedef void __fastcall TransactItem_t(
-DWORD type, 
-DWORD gold_give,
-TransactionInfo give,
-DWORD gold_recv,
-TransactionInfo recv
+	DWORD type, 
+	DWORD gold_give,
+	GW::TransactionInfo give,
+	DWORD gold_recv,
+	GW::TransactionInfo recv
 );
 
 typedef void __fastcall RequestQuote_t(
-DWORD type, 
-DWORD unknown,
-QuoteInfo give,
-QuoteInfo recv
+	DWORD type, 
+	DWORD unknown,
+	GW::QuoteInfo give,
+	GW::QuoteInfo recv
 );
 
 TransactItem_t* g_transactitemfn;
 RequestQuote_t* g_requestquotefn;
 
-DWORD           g_lastitemid;
-DWORD           g_lastitemprice;
+//DWORD           g_lastitemid;
+//DWORD           g_lastitemprice;
 
 GW::MerchantMgr::MerchantMgr() : GWCAManager() {
     PatternScanner scan(0x401000, 0x4FF000);
     
-    g_transactitemfn = reinterpret_cast<TransactItem_t*>(scan.FindPattern("\x8B\x45\x18\x83\xF8\x10\x76\x17\x68", "xxxxxxxxx", -0x2C));
-    
-    if (transaction_function_) {
-        printf("transaction_function_ = %X\n", transaction_function_);
+	g_transactitemfn = reinterpret_cast<TransactItem_t*>(scan.FindPattern("\x8B\x45\x18\x83\xF8\x10\x76\x17\x68", "xxxxxxxxx", -0x2C));
+    if (g_transactitemfn) {
+        printf("transaction_function_ = %X\n", g_transactitemfn);
     } else {
         printf("transaction_function_ = ERR\n");
     }
     
     g_requestquotefn = reinterpret_cast<RequestQuote_t*>(scan.FindPattern("\x53\x56\x8B\x75\x0C\x57\x83\xFE\x10", "xxxxxxxxx", -0x9));
-    
-    if (quote_function_) {
-        printf("quote_function_ = %X\n", transaction_function_);
+    if (g_requestquotefn) {
+        printf("quote_function_ = %X\n", g_requestquotefn);
     } else {
         printf("quote_function_ = ERR\n");
     }
     
-    StoCMgr::Instance().AddGameServerEvent<P235_QuotedItemPrice>([](P235_QuotedItemPrice* p) -> bool {
-                                                                 g_lastitemid    = p->itemid;
-                                                                 g_lastitemprice = p->price;
-                                                                 return false;
-                                                                 }
-                                                                 );
+	// Uncomment to have the manager same the item quotes, otherwise add this callback yourself
+	//StoCMgr::Instance().AddGameServerEvent<GW::Packet::StoC::P235_QuotedItemPrice>(
+	//	[](GW::Packet::StoC::P235_QuotedItemPrice* p) -> bool {
+	//	g_lastitemid    = p->itemid;
+	//	g_lastitemprice = p->price;
+	//	return false;
+	//});
 }
 
-void GW::MerchantMgr::RestoreHooks() {}
+void GW::MerchantMgr::TransactItems(TransactionType type, 
+	DWORD gold_give, TransactionInfo give, 
+	DWORD gold_recv, TransactionInfo recv) {
 
-void 
-GW::MerchantMgr::TransactItems(
-TransactionType type,
-DWORD gold_give,
-TransactionInfo give,
-DWORD gold_recv,
-TransactionInfo recv
-) {
-    
-    g_transactitemfn(
-        type,
+    g_transactitemfn(type,
         gold_give,
         give,
         gold_recv,
-        recv
-        );
+        recv);
     
 }
 
-void
-GW::MerchantMgr::RequestQuote(
-TransactionType type,
-QuoteInfo give,
-QuoteInfo recv
-) {
+void GW::MerchantMgr::RequestQuote(TransactionType type, 
+	QuoteInfo give, QuoteInfo recv) {
     
-    g_lastitemprice = 0;
-    g_requestquotefn(
-        type,
+    //g_lastitemprice = 0;
+    g_requestquotefn(type,
         0,
         give,
-        recv
-        );
+        recv);
     
 }
 
-DWORD 
-GW::MerchantMgr::GetQuotedItemPrice() {
-    return g_lastitemprice;
-}
+//DWORD GW::MerchantMgr::GetQuotedItemPrice() {
+//    return g_lastitemprice;
+//}

3cd45e3004bc7c2662bcc80b62fc2e67223aea5d
Greg Lando <kaos4d1@gmail.com>
2017-02-18 17:25:51 -0800

    - changed batfile a bit

--- make.bat
+++ make.bat
@@ -1,7 +1,12 @@
 @echo off
-call vcvarsall
 
-cl /c /Zi Dependencies\disasm\ld32.c
-for /R GWCA\src\ %%G in (*.cpp) do cl /EHsc /DUNICODE /c %%G
+if not defined DevEnvDir (
+
+call "C:\Program Files (x86)\Microsoft Visual Studio 12.0\VC\vcvarsall.bat" x86
+
+)
+
+cl /nologo /c /Zi Dependencies\disasm\ld32.c
+for /R GWCA\src\ %%G in (*.cpp) do cl /nologo /EHsc /I.\ /DUNICODE /c %%G
 lib -nologo -out:GWCA.lib *.obj
 del *.obj

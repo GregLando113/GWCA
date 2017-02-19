533cbcc67b43fba0d8e2031e896ea71f4dc3de19
Greg Lando <kaos4d1@gmail.com>
2017-02-18 19:05:49 -0800

    - did things i guess?

--- GWCA/src/MerchantMgr.cpp
+++ GWCA/src/MerchantMgr.cpp
@@ -8,18 +8,18 @@
 
 
 typedef void __fastcall TransactItem_t(
-	DWORD type, 
-	DWORD gold_give,
-	GW::TransactionInfo give,
-	DWORD gold_recv,
-	GW::TransactionInfo recv
+DWORD type, 
+DWORD gold_give,
+GW::TransactionInfo give,
+DWORD gold_recv,
+GW::TransactionInfo recv
 );
 
 typedef void __fastcall RequestQuote_t(
-	DWORD type, 
-	DWORD unknown,
-	GW::QuoteInfo give,
-	GW::QuoteInfo recv
+DWORD type, 
+DWORD unknown,
+GW::QuoteInfo give,
+GW::QuoteInfo recv
 );
 
 TransactItem_t* g_transactitemfn;
@@ -31,7 +31,7 @@ RequestQuote_t* g_requestquotefn;
 GW::MerchantMgr::MerchantMgr() : GWCAManager() {
     PatternScanner scan(0x401000, 0x4FF000);
     
-	g_transactitemfn = reinterpret_cast<TransactItem_t*>(scan.FindPattern("\x8B\x45\x18\x83\xF8\x10\x76\x17\x68", "xxxxxxxxx", -0x2C));
+    g_transactitemfn = reinterpret_cast<TransactItem_t*>(scan.FindPattern("\x8B\x45\x18\x83\xF8\x10\x76\x17\x68", "xxxxxxxxx", -0x2C));
     if (g_transactitemfn) {
         printf("transaction_function_ = %X\n", g_transactitemfn);
     } else {
@@ -45,35 +45,35 @@ GW::MerchantMgr::MerchantMgr() : GWCAManager() {
         printf("quote_function_ = ERR\n");
     }
     
-	// Uncomment to have the manager same the item quotes, otherwise add this callback yourself
-	//StoCMgr::Instance().AddGameServerEvent<GW::Packet::StoC::P235_QuotedItemPrice>(
-	//	[](GW::Packet::StoC::P235_QuotedItemPrice* p) -> bool {
-	//	g_lastitemid    = p->itemid;
-	//	g_lastitemprice = p->price;
-	//	return false;
-	//});
+    // Uncomment to have the manager same the item quotes, otherwise add this callback yourself
+    //StoCMgr::Instance().AddGameServerEvent<GW::Packet::StoC::P235_QuotedItemPrice>(
+    //	[](GW::Packet::StoC::P235_QuotedItemPrice* p) -> bool {
+    //	g_lastitemid    = p->itemid;
+    //	g_lastitemprice = p->price;
+    //	return false;
+    //});
 }
 
 void GW::MerchantMgr::TransactItems(TransactionType type, 
-	DWORD gold_give, TransactionInfo give, 
-	DWORD gold_recv, TransactionInfo recv) {
-
+                                    DWORD gold_give, TransactionInfo give, 
+                                    DWORD gold_recv, TransactionInfo recv) {
+    
     g_transactitemfn(type,
-        gold_give,
-        give,
-        gold_recv,
-        recv);
+                     gold_give,
+                     give,
+                     gold_recv,
+                     recv);
     
 }
 
 void GW::MerchantMgr::RequestQuote(TransactionType type, 
-	QuoteInfo give, QuoteInfo recv) {
+                                   QuoteInfo give, QuoteInfo recv) {
     
     //g_lastitemprice = 0;
     g_requestquotefn(type,
-        0,
-        give,
-        recv);
+                     0,
+                     give,
+                     recv);
     
 }
 

694df0b6109d80d8212773a9fdc05fb671e56999
reduf <laurent.dufresne@hotmail.com>
2017-08-06 10:34:10 -0400

    Remove useless implementation.

--- GWCA_DLL/GWCA.cpp
+++ GWCA_DLL/GWCA.cpp
@@ -25,51 +25,6 @@
 
 using namespace GW;
 
-GWAPI GW::Agent* GWCA::GetPlayer() { return Agents::GetPlayer(); }
-GWAPI GW::Agent* GWCA::GetTarget() { return Agents::GetTarget(); }
-
-GWAPI GW::Agent* GWCA::GetAgentByID(DWORD id) { return Agents::GetAgentByID(id); }
-
-// Get Current AgentID's of player or target.
-GWAPI DWORD GWCA::GetPlayerId() { return Agents::GetPlayerId(); }
-GWAPI DWORD GWCA::GetTargetId() { return Agents::GetTargetId(); }
-
-GWAPI void GWCA::SendChat(char channel, const wchar *msg) {
-	GW::Chat::SendChat(msg, channel);
-}
-
-GWAPI void GWCA::SendChat(char channel, const char *msg) {
-	GW::Chat::SendChat(msg, channel);
-}
-
-GWAPI void GWCA::PrintChat(Channel channel, const wchar *msg) {
-	GW::Chat::WriteChat((GW::Chat::Channel)channel, msg);
-}
-
-GWAPI void GWCA::CreateCommand(std::wstring cmd, CmdCB callback) {
-    // GW::Chat::RegisterCommand(cmd, callback);
-}
-
-GWAPI void GWCA::DeleteCommand(std::wstring cmd) {
-	// GW::Chat::DeleteCommand(cmd);
-}
-
-static void (*_OnEvent)(GWCA::Event e, ...);
-
-static bool _OnLocalMessage(int chan, wchar *msg) {
-    _OnEvent(GWCA::Event::OnChatMessage, chan, L"", msg);
-	return false;
-}
-
-static void _OnWhisperMessage(const wchar sender[20], const wchar msg[140]) {
-    _OnEvent(GWCA::Event::OnChatMessage, GWCA::CHANNEL_WHISPER, sender, msg);
-}
-
-static bool _OnPacketRecv(GW::Packet::StoC::PacketBase *p) {
-    _OnEvent(GWCA::Event::OnServerMessage, p->header, p);
-	return false;
-}
-
 DWORD WINAPI DllMain(HMODULE hMod, DWORD dwReason, LPVOID lpReserved) {
     if (dwReason == DLL_PROCESS_ATTACH) {
         if(!GW::Initialize())

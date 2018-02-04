281c57ccab02172e0bb257828ed7ffae6b6f8371
reduf <laurent.dufresne@hotmail.com>
2018-02-04 11:18:54 -0500

    Added fields to NPC

--- GWCA/GameEntities/NPC.h
+++ GWCA/GameEntities/NPC.h
@@ -11,14 +11,17 @@ namespace GW {
         /* +h0000 */ DWORD ModelFileID;
         /* +h0004 */ DWORD h0004;
         /* +h0008 */ DWORD scale; // I think, 2 highest order bytes are percent of size, so 0x64000000 is 100%
-        /* +h000C */ DWORD h000C;
+        /* +h000C */ DWORD Sex;
         /* +h0010 */ DWORD NpcFlags;
-        /* +h0014 */ DWORD Profession;
-        /* +h0018 */ DWORD h0018[2];
+        /* +h0014 */ DWORD Primary;
+        /* +h0018 */ DWORD h0018;
+		/* +h001C */ BYTE DefaultLevel;
+		// +h001D    BYTE padding;
+		// +h001E    WORD padding;
         /* +h0020 */ wchar *NameString;
         /* +h0024 */ DWORD *ModelFiles;
-        /* +h0028 */ DWORD h0028; // probably length of ModelFile
-        /* +h002C */ DWORD h002C; // probably capacity of ModelFile
+        /* +h0028 */ DWORD lengtSize; // length of ModelFile
+        /* +h002C */ DWORD capacity; // capacity of ModelFile
     };
     using NPCArray = Array<NPC>;
 }

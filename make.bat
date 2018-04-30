7039eb76f655aa9651b7db17fdc4feeeb3d2a270
Greg <greglando@gmail.com>
2018-04-30 12:07:32 -0700

    make.bat updated to compile minhook

--- make.bat
+++ make.bat
@@ -1,11 +1,11 @@
 @echo off
 
 if not defined DevEnvDir (
-
 call "C:\Program Files (x86)\Microsoft Visual Studio 14.0\VC\vcvarsall.bat" x86
-
 )
 
 for /R GWCA\src\ %%G in (*.cpp) do cl /nologo /O2 /EHsc /I.\ /DUNICODE /c %%G
+for /R Dependencies\minhook\src\ %%G in (*.c) do cl /nologo /O2 /EHsc /I.\ /DUNICODE /c %%G
+for /R Dependencies\minhook\src\hde\ %%G in (*.c) do cl /nologo /O2 /EHsc /I.\ /DUNICODE /c %%G
 lib -nologo -out:GWCA.lib *.obj
 del *.obj

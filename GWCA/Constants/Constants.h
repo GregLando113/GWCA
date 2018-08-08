fb320fe4366eff67cdecd67105c70e5b451dea5a
reduf <laurent.dufresne@hotmail.com>
2018-08-08 19:29:01 -0400

    Jora had bad HeroId

--- GWCA/Constants/Constants.h
+++ GWCA/Constants/Constants.h
@@ -93,9 +93,9 @@ namespace GW {
         enum HeroID : DWORD {
             NoHero, Norgu, Goren, Tahlkora, MasterOfWhispers, AcolyteJin, Koss,
             Dunkoro, AcolyteSousuke, Melonni, ZhedShadowhoof, GeneralMorgahn,
-            MargridTheSly, Zenmai, Olias, Razah, MOX, Jora, KeiranThackeray, 
-			PyreFierceshot, Anton, Livia, Hayda, Kahmu, Gwen, Xandra, Vekk, 
-			Ogden, Merc1, Merc2, Merc3, Merc4, Merc5, Merc6, Merc7, Merc8, 
+            MargridTheSly, Zenmai, Olias, Razah, MOX, KeiranThackeray, Jora,
+			PyreFierceshot, Anton, Livia, Hayda, Kahmu, Gwen, Xandra, Vekk,
+			Ogden, Merc1, Merc2, Merc3, Merc4, Merc5, Merc6, Merc7, Merc8,
 			Miku, ZeiRi
         };
 

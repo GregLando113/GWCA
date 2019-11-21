a2683b68ca689e6eb18771792100b454edb7bfed
Jon Riley <jon@3vcloud.uk>
2019-11-21 23:33:45 +0000

    Added Party invite StoC headers Fixed AcceptPartyRequest CtoS header

--- Include/GWCA/Packets/StoC.h
+++ Include/GWCA/Packets/StoC.h
@@ -557,6 +557,7 @@ namespace GW {
             };
             const uint32_t Packet<ErrorMessage>::STATIC_HEADER = 0x1C1;
 
+			/* Party Invites */
             struct PartyHenchmanAdd : Packet<PartyHenchmanAdd> {
                 uint32_t party_id;
                 uint32_t agent_id;
@@ -572,25 +573,89 @@ namespace GW {
             };
             const uint32_t Packet<PartyHenchmanRemove>::STATIC_HEADER = 0x1C5;
 
+			struct PartyHeroAdd : Packet<PartyHeroAdd> {
+				uint32_t party_id;
+				uint32_t owner_player_number;
+				uint32_t agent_id;
+				uint32_t hero_id;
+				uint32_t level;
+			};
+			const uint32_t Packet<PartyHeroAdd>::STATIC_HEADER = 0x1C7;
 
+			struct PartyHeroRemove : Packet<PartyHeroRemove> {
+				uint32_t party_id;
+				uint32_t owner_player_number;
+				uint32_t agent_id;
+			};
+			const uint32_t Packet<PartyHeroRemove>::STATIC_HEADER = 0x1C8;
+
+			
+			// Invite sent to another party
+			struct PartyInviteSent_Create : Packet<PartyInviteSent_Create> {
+				uint32_t target_party_id;
+			};
+			const uint32_t Packet<PartyInviteSent_Create>::STATIC_HEADER = 0x1C9;
+			// Invite received from another party
+			struct PartyInviteReceived_Create : Packet<PartyInviteReceived_Create> {
+				uint32_t target_party_id; // word
+			};
+			const uint32_t Packet<PartyInviteReceived_Create>::STATIC_HEADER = 0x1CA;
+
+			// Remove a sent party invite. Invitation has been cancelled.
+			struct PartyInviteSent_Cancel : Packet<PartyInviteSent_Cancel> {
+				uint32_t target_party_id; // word
+			};
+			const uint32_t Packet<PartyInviteSent_Cancel>::STATIC_HEADER = 0x1CB;
+
+			// Remove a received party invite. Invitation has been cancelled.
+			struct PartyInviteReceived_Cancel : Packet<PartyInviteReceived_Cancel> {
+				uint32_t target_party_id; // word
+			};
+			const uint32_t Packet<PartyInviteReceived_Cancel>::STATIC_HEADER = 0x1CC;
+
+			// Remove a received party invite. Invitation has been either accepted or rejected (we dont know atm)
+			struct PartyInviteSent_Response : Packet<PartyInviteSent_Response> {
+				uint32_t target_party_id; // word
+			};
+			const uint32_t Packet<PartyInviteSent_Response>::STATIC_HEADER = 0x1CD;
+
+			// Remove a sent party invite. Invitation has been either accepted or rejected (we dont know atm)
+			struct PartyInviteReceived_Response : Packet<PartyInviteReceived_Response> {
+				uint32_t target_party_id; // word
+			};
+			const uint32_t Packet<PartyInviteReceived_Response>::STATIC_HEADER = 0x1CE;
+
+			// A player in a party has been updated or added. Not necessarily yours.
             struct PartyPlayerAdd : Packet<PartyPlayerAdd> {
-                uint32_t party_id; // word
-                uint32_t player_id; // word
-                uint32_t state; // byte
+                uint32_t party_id;
+                uint32_t player_id;
+                uint32_t state; // 3 = Invited
             };
             const uint32_t Packet<PartyPlayerAdd>::STATIC_HEADER = 0x1D0;
-
+			// Player has left party. Not necessarily yours.
             struct PartyPlayerRemove : Packet<PartyPlayerRemove> {
                 uint32_t party_id;
                 uint32_t player_id;
             };
             const uint32_t Packet<PartyPlayerRemove>::STATIC_HEADER = 0x1D5;
+			// Player in party has toggled ready. Not necessarily yours.
+			struct PartyPlayerReady : Packet<PartyPlayerReady> {
+				uint32_t party_id;
+				uint32_t player_id;
+				uint32_t is_ready;
+			};
+			const uint32_t Packet<PartyPlayerReady>::STATIC_HEADER = 0x1D6;
 
+			// When a new party is created:
+			// 1.	PartyPlayerStreamStart packet is sent
+			// 2.	PartyPlayerAdd packet per member
+			//		PartyHeroAdd packet per hero
+			//		PartyHenchmanAdd packer per henchman
+			// 3. PartyPlayerStreamEnd packet is sent
 			struct PartyPlayerStreamStart : Packet<PartyPlayerStreamStart> {
 				uint32_t party_id; // word
 			};
 			const uint32_t Packet<PartyPlayerStreamStart>::STATIC_HEADER = 0x1D7;
-
 			struct PartyPlayerStreamEnd : Packet<PartyPlayerStreamEnd> {
 				uint32_t party_id; // word
 			};

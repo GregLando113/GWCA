e6cb4038b5322f20e336c325c2d768174add3d8d
Daniel Farley <dfarley1@users.noreply.github.com>
2019-12-22 01:27:56 -0800

    Add Zaishen Mission QuestIDs
    Taken from GWW

--- Include/GWCA/Constants/QuestIDs.h
+++ Include/GWCA/Constants/QuestIDs.h
@@ -47,6 +47,63 @@ namespace GW {
                 constexpr int FoundryBreakout = 743;
                 constexpr int FoundryOfFailedCreations = 744;
             }
+
+            namespace ZaishenMission {
+                // Prophecies
+                constexpr uint32_t Fort_Ranik = 937;
+                constexpr uint32_t Ruins_of_Surmia = 938;
+                constexpr uint32_t Borlis_Pass = 940;
+                constexpr uint32_t The_Frost_Gate = 941;
+                constexpr uint32_t Gates_of_Kryta = 942;
+                constexpr uint32_t DAlessio_Seaboard = 943;
+                constexpr uint32_t Divinity_Coast = 944;
+                constexpr uint32_t The_Wilds = 945;
+                constexpr uint32_t Bloodstone_Fen = 946;
+                constexpr uint32_t Riverside_Province = 948;
+                constexpr uint32_t Sanctum_Cay = 949;
+                constexpr uint32_t Dunes_of_Despair = 950;
+                constexpr uint32_t Thirsty_River = 951;
+                constexpr uint32_t Elona_Reach = 952;
+                constexpr uint32_t Augury_Rock = 953;
+                constexpr uint32_t The_Dragons_Lair = 954;
+                constexpr uint32_t Iron_Mines_of_Moladune = 956;
+                constexpr uint32_t Thunderhead_Keep = 957;
+                constexpr uint32_t Ring_of_Fire = 958;
+                constexpr uint32_t Abaddons_Mouth = 959;
+                constexpr uint32_t Hells_Precipice = 960;
+                // Factions
+                constexpr uint32_t Zen_Daijun = 961;
+                constexpr uint32_t Naphui_Quarter = 963;
+                constexpr uint32_t Boreas_Seabed = 966;
+                constexpr uint32_t The_Eternal_Grove = 968;
+                constexpr uint32_t Unwaking_Waters = 969;
+                constexpr uint32_t Gyala_Hatchery = 970;
+                constexpr uint32_t Raisu_Palace = 971;
+                constexpr uint32_t Imperial_Sanctum = 972;
+                constexpr uint32_t Tahnnakai_Temple = 980; // CONFIRM
+                // Nightfall
+                constexpr uint32_t Chabek_Village = 978;
+                constexpr uint32_t Jokanur_Diggings = 979;
+                constexpr uint32_t Blacktide_Den = 980; // CONFIRM
+                constexpr uint32_t Consulate_Docks = 981;
+                constexpr uint32_t Venta_Cemetary = 982;
+                constexpr uint32_t Kodonur_Crossroads = 983;
+                constexpr uint32_t Pogahn_Passage = 1181;
+                constexpr uint32_t Moddok_Crevice = 985;
+                constexpr uint32_t Dzagonur_Bastion = 987;
+                constexpr uint32_t Grand_Court_of_Sebelkeh = 989;
+                constexpr uint32_t Jennurs_Horde = 990;
+                constexpr uint32_t Nundu_Bay = 991;
+                constexpr uint32_t Ruins_of_Morah = 993;
+                constexpr uint32_t Abaddons_Gate = 996;
+                // Eye of the North
+                constexpr uint32_t G_O_L_E_M = 1002;
+                constexpr uint32_t Assault_on_the_Stronghold = 1005;
+                constexpr uint32_t Curse_of_the_Nornbear = 1006;
+                constexpr uint32_t A_Gate_Too_Far = 1008;
+                constexpr uint32_t Destructions_Depths = 1009;
+                constexpr uint32_t A_Time_for_Heroes = 1010;
+            }
         }
     }
 }

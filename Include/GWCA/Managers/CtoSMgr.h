542cdb822dadec4e1b17fc574bd0d5b998afd471
Jon <jon@3vcloud.uk>
2020-04-23 13:42:46 +0100

    Added CtoS hooks in CtoSMgr

--- Include/GWCA/Managers/CtoSMgr.h
+++ Include/GWCA/Managers/CtoSMgr.h
@@ -1,15 +1,29 @@
 #pragma once
 
 #include <GWCA/Utilities/Export.h>
+#include <GWCA/Utilities/Hook.h>
+#include <GWCA/Utilities/Hooker.h>
+#include <GWCA/Utilities/Macros.h>
+#include <GWCA/Utilities/Scanner.h>
+
+#include <GWCA/Managers/Module.h>
+
 
 namespace GW {
     struct Module;
     extern Module CtoSModule;
 
     namespace CtoS {
+        typedef HookCallback<void*> PacketCallback;
         // Send packet that uses only dword parameters, can copypaste most gwa2 sendpackets :D
         GWCA_API void SendPacket(uint32_t size, ...);
         
+        GWCA_API void RegisterPacketCallback(
+            HookEntry* entry,
+            uint32_t header,
+            PacketCallback callback);
+
+        GWCA_API void RemoveCallback(uint32_t header, HookEntry* entry);
         // Send a packet with a specific struct alignment, used for more complex packets.
         GWCA_API void SendPacket(uint32_t size, void* buffer);
 
--- Source/CtoSMgr.cpp
+++ Source/CtoSMgr.cpp
@@ -1,26 +1,41 @@
 #include "stdafx.h"
 
-#include <GWCA/Utilities/Export.h>
-#include <GWCA/Utilities/Macros.h>
-#include <GWCA/Utilities/Scanner.h>
 
-#include <GWCA/Managers/Module.h>
 #include <GWCA/Managers/CtoSMgr.h>
-
 namespace {
     using namespace GW;
 
     typedef void(__cdecl *SendPacket_pt)(
         uint32_t context, uint32_t size, void* packet);
     SendPacket_pt SendPacket_Func;
-
+    SendPacket_pt RetSendPacket;
+    std::vector<std::unordered_map<HookEntry*, CtoS::PacketCallback>> packets_callbacks;
+    bool __cdecl CtoSHandler_Func(uint32_t context, uint32_t size, void* packet) {
+        HookBase::EnterHook();
+        HookStatus status;
+        uint32_t header = *(uint32_t*)packet;
+        if (header < packets_callbacks.size()) {
+            auto callbacks = packets_callbacks.at(header);
+            for (auto& it : packets_callbacks[header]) {
+                it.second(&status, packet);
+                ++status.altitude;
+            }
+        }
+        if (!status.blocked)
+            RetSendPacket(context, size, packet);
+        GW::HookBase::LeaveHook();
+        return true;
+    }
     uintptr_t game_srv_object_addr;
+
+    
 
     void Init() {
         SendPacket_Func = (SendPacket_pt)Scanner::Find(
             "\xF7\xD9\xC7\x47\x54\x01\x00\x00\x00\x1B\xC9\x81", "xxxxxxxxxxxx", -0xC3);
         printf("[SCAN] SendPacket = %p\n", SendPacket_Func);
-
+        if (Verify(SendPacket_Func))
+            HookBase::CreateHook(SendPacket_Func, CtoSHandler_Func, (void**)&RetSendPacket);
         {
             uintptr_t address = Scanner::Find(
                 "\xC3\xA1\x00\x00\x00\x00\x85\xC0\x74\xF1", "xx????xxxx", +2);
@@ -28,6 +43,11 @@ namespace {
             if (Verify(address))
                 game_srv_object_addr = *(uintptr_t *)address;
         }
+        packets_callbacks.resize(180);
+    }
+    void Exit() {
+        if (SendPacket_Func)
+            HookBase::RemoveHook(SendPacket_Func);
     }
 }
 
@@ -37,11 +57,23 @@ namespace GW {
         "CtoSModule",   // name
         NULL,           // param
         ::Init,         // init_module
-        NULL,           // exit_module
+        ::Exit,           // exit_module
         NULL,           // enable_hooks
         NULL,           // disable_hooks
     };
-
+    void CtoS::RegisterPacketCallback(
+        HookEntry* entry,
+        uint32_t header,
+        PacketCallback callback)
+    {
+        packets_callbacks[header].insert({ entry, callback });
+    }
+    void CtoS::RemoveCallback(uint32_t header, HookEntry* entry) {
+        auto& callbacks = packets_callbacks[header];
+        auto it = callbacks.find(entry);
+        if (it != callbacks.end())
+            callbacks.erase(it);
+    }
     void CtoS::SendPacket(uint32_t size, void *buffer) {
         if (Verify(SendPacket_Func && game_srv_object_addr)) {
             SendPacket_Func(*(uint32_t *)game_srv_object_addr, size, buffer);

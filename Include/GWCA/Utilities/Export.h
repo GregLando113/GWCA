58d0d48922232e04c86bc0b985334d53ca12038f
DubbleClick <m@pyc.ac>
2022-10-03 02:11:25 +0200

    remove useless extern declaration for GWCA_API

--- Include/GWCA/Utilities/Export.h
+++ Include/GWCA/Utilities/Export.h
@@ -9,11 +9,11 @@
 #endif
 
 #ifdef GWCA_BUILD_EXPORTS
-#define GWCA_API DllExport
+# define GWCA_API DllExport
 #else
 #ifdef GWCA_IMPORT
-#define GWCA_API DllImport
+# define GWCA_API DllImport
 #else
-# define GWCA_API extern
+# define GWCA_API
 #endif
 #endif

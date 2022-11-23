26bbd49c7098e4deda224563221f60d35469ee2f
hazyPixels <eh2th-hazyPixels@yahoo.com>
2022-11-22 21:31:02 -0800

    Add ability to hide all enemy agents

--- Examples/HideAgent/main.cpp
+++ Examples/HideAgent/main.cpp
@@ -1,6 +1,7 @@
 #define WIN32_LEAN_AND_MEAN
 #include <Windows.h>
 
+#include <map>
 #include <string>
 
 #include <GWCA/GWCA.h>
@@ -11,6 +12,7 @@
 #include <GWCA/GameEntities/Agent.h>
 #include <GWCA/Managers/AgentMgr.h>
 #include <GWCA/Managers/ChatMgr.h>
+#include <GWCA/Managers/MapMgr.h>
 #include <GWCA/Managers/RenderMgr.h>
 
 // We can forward declare, because we won't use it
@@ -18,17 +20,98 @@ struct IDirect3DDevice9;
 
 static volatile bool running;
 
-static void CmdHideAgent(const wchar_t *msg, int argc, wchar_t **argv)
+std::map<GW::AgentID, std::pair<float, float> > hiddenAgents;
+GW::Constants::Allegiance hideAllegiance = (GW::Constants::Allegiance) 0;
+
+inline void HideAgent(GW::Agent* agt)
 {
-    GW::Agent * target = GW::Agents::GetTarget();
-    if (!target)
+    if (!agt) return;
+    if (agt->width2 > 0 && agt->height2 > 0)
     {
-        GW::Chat::WriteChat(GW::Chat::CHANNEL_MODERATOR, L"Please select a target first");
+        hiddenAgents[agt->agent_id] = std::pair<float, float>(agt->width2, agt->height2);
+        agt->width2 = agt->height2 = 0;
+    }
+}
+
+void HideAgents()
+{
+    auto agents = GW::Agents::GetAgentArray();
+    if (!agents) return;
+    for (auto agt : *agents)
+    {
+        if (!agt) continue;
+        // hide any previously hidden agents that may have been out of range
+        if (agt->width2 > 0 || agt->height2 > 0)
+        {
+            auto it = hiddenAgents.find(agt->agent_id);
+            if (it != hiddenAgents.end())
+            {
+                HideAgent(agt);
+                continue;
+            }
+        }
+        // hide any agents that match allegiance criteria
+        auto agtLiving = agt->GetAsAgentLiving();
+        if (!agtLiving) continue;
+        if (agtLiving->allegiance != hideAllegiance) continue;
+        HideAgent(agt);
+    }
+}
+
+inline void UnhideAgent(GW::Agent* agt)
+{
+    if (!agt) return;
+    if (agt->width2 > 0 && agt->height2 > 0) return;
+    auto it = hiddenAgents.find(agt->agent_id);
+    if (it == hiddenAgents.end()) return;
+    auto& pair = it->second;
+    agt->width2 = pair.first;
+    agt->height2 = pair.second;
+    hiddenAgents.erase(it);
+}
+
+void UnhideAgents()
+{
+    auto agents = GW::Agents::GetAgentArray();
+    if (!agents) return;
+    for (auto agt : *agents)
+    {
+        if (!agt) continue;
+        UnhideAgent(agt);
+    }
+}
+
+static void CmdHide(const wchar_t* msg, int argc, wchar_t** argv)
+{
+    if (argc < 2)
+    {
+        GW::Agent* target = GW::Agents::GetTarget();
+        if (!target)
+        {
+            GW::Chat::WriteChat(GW::Chat::CHANNEL_MODERATOR, L"Please select a target first");
+            return;
+        }
+
+        HideAgent(target);
         return;
     }
 
-    target->width2 = target->height2 = 0;
-
+    std::wstring ws(argv[1]);
+    if (ws == L"enemies")
+    {
+        hideAllegiance = (GW::Constants::Allegiance::Enemy);
+        GW::Chat::WriteChat(GW::Chat::CHANNEL_MODERATOR, L"hiding enemies...");
+    }
+    else if (ws == L"none")
+    {
+        GW::Chat::WriteChat(GW::Chat::CHANNEL_MODERATOR, L"Unhiding all agents...");
+        hideAllegiance = (GW::Constants::Allegiance) 0;
+        UnhideAgents();
+    }
+    else
+    {
+        GW::Chat::WriteChat(GW::Chat::CHANNEL_MODERATOR, L"Usage: \"/hide\" or \"/hide enemies\" or \"/hide none\"");
+    }
 }
 
 static void GameLoop(IDirect3DDevice9* device)
@@ -36,18 +119,31 @@ static void GameLoop(IDirect3DDevice9* device)
     // This is call from within the game thread and all operation should be done here.
     // You can't freeze this thread, so no blocking operation or at your own risk.
     static bool initialized = false;
-    if (!initialized) {
-        GW::Chat::CreateCommand(L"hide", CmdHideAgent);
+    if (!initialized) 
+    {
+        GW::Chat::CreateCommand(L"hide", CmdHide);
         initialized = true;
-
-        GW::Chat::WriteChat(GW::Chat::CHANNEL_MODERATOR, L"HideAgent: Initialized.. press the END key to quit.");
+        GW::Chat::WriteChat(GW::Chat::CHANNEL_MODERATOR, L"HideAgent: Initialized. Press the END key to quit.");
     }
-
-    if (GetAsyncKeyState(VK_END) & 1) {
+    if (GetAsyncKeyState(VK_END) & 1) 
+    {
         GW::Chat::WriteChat(GW::Chat::CHANNEL_MODERATOR, L"HideAgent: Bye!");
         GW::DisableHooks();
         running = false;
     }
+
+    if (GW::Map::GetInstanceType() == GW::Constants::InstanceType::Loading)
+    {
+        hiddenAgents.clear();
+    }
+    else
+    {
+        if (running)
+            HideAgents();
+        else
+            UnhideAgents();
+    }
+
 }
 
 static DWORD WINAPI ThreadProc(LPVOID lpModule)

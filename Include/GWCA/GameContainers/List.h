78bc78fec48e9a91781f8cc19265a84d94ea7809
DubbleClick <m@pyc.ac>
2023-03-26 18:24:48 +0200

    make TList type iterable

--- Include/GWCA/GameContainers/List.h
+++ Include/GWCA/GameContainers/List.h
@@ -4,41 +4,45 @@ namespace GW {
 
     template <typename T>
     struct TLink {
-
         bool IsLinked() const { return prev_link != this; }
 
-        void Unlink() {
+        void Unlink()
+        {
             RemoveFromList();
 
             next_node = (T*)((size_t)this + 1);
             prev_link = this;
         }
 
-        T *Prev() {
-            T *prevNode = prev_link->prev_link->next_node;
+        T* Prev()
+        {
+            T* prevNode = prev_link->prev_link->next_node;
             if ((size_t)prevNode & 1)
-                return NULL;
+                return nullptr;
             return prevNode;
         }
 
-        T *Next() {
+        T* Next()
+        {
             if ((size_t)next_node & 1)
-                return NULL;
+                return nullptr;
             return next_node;
         }
 
-        TLink<T> *NextLink() {
-            size_t offset = (size_t)this - ((size_t)prev_link->next_node & ~1);
-            return (TLink<T>*) ( ((size_t)next_node & ~1) + offset );
+        TLink* NextLink()
+        {
+            const size_t offset = (size_t)this - ((size_t)prev_link->next_node & ~1);
+            return (TLink*)(((size_t)next_node & ~1) + offset);
         }
 
-        TLink<T> *PrevLink() { return prev_link; }
+        TLink* PrevLink() { return prev_link; }
 
     protected:
-        TLink<T> *prev_link; // +h0000
-        T        *next_node; // +h0004
+        TLink* prev_link; // +h0000
+        T* next_node;     // +h0004
 
-        void RemoveFromList() {
+        void RemoveFromList()
+        {
             NextLink()->prev_link = prev_link;
             prev_link->next_node = next_node;
         }
@@ -46,11 +50,65 @@ namespace GW {
 
     template <typename T>
     struct TList {
-        TLink<T>* Get() {
-            return &link;
+        class iterator {
+        public:
+            using difference_type = std::ptrdiff_t;
+            using value_type = T;
+
+            iterator()
+                : current(nullptr)
+            {
+            }
+            explicit iterator(TLink<T>* node)
+                : current(node)
+            {
+            }
+
+            T& operator*() { return *current->Next(); }
+            T* operator->() { return current->Next(); }
+            T& operator*() const { return *current->Next(); }
+            T* operator->() const { return current->Next(); }
+
+            iterator& operator++()
+            {
+                current = current->NextLink();
+                return *this;
+            }
+
+            iterator operator++(int)
+            {
+                iterator it(current);
+                ++*this;
+                return it;
+            }
+
+            bool operator==(const iterator& other) const { return current == other.current; }
+
+            bool operator!=(const iterator& other) const { return !(*this == other); }
+
+        private:
+            TLink<T>* current;
+        };
+
+        iterator begin() { return iterator(&link); }
+
+        iterator end()
+        {
+            TLink<T>* last = &link;
+            while (last->Next() != nullptr && last->NextLink() != &link) {
+                last = last->NextLink();
+            }
+            iterator it(last);
+            return last->NextLink() == &link ? it : ++it;
         }
+
+        [[nodiscard]] iterator begin() const { return iterator(&link); }
+        [[nodiscard]] iterator end() const { return end(); }
+
+        TLink<T>* Get() { return &link; }
+
     protected:
-        size_t   offset;
+        size_t offset{};
         TLink<T> link;
     };
-}
+} // namespace GW
